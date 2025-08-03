b5cb6eaa7ff2c534ade3e6530532f703de9b03b6
Daniel O'Connor <daniel.oconnor@gmail.com>
2025-08-03 03:15:46 +0000

    Add sketch of AI generated PR from https://github.com/fatfreecrm/fat_free_crm/pull/1518

---
+++ ffcrm_webhook_read_ai.gemspec
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+$LOAD_PATH.push File.expand_path('lib', __dir__)
+
+Gem::Specification.new do |gem|
+  gem.name = 'ffcrm_webhook_read_ai'
+  gem.authors = ["Daniel O'Connor"]
+  gem.summary = 'Fat Free CRM'
+  gem.description = 'An open source, Ruby on Rails customer relationship management platform'
+  gem.homepage = 'http://fatfreecrm.com'
+  gem.email = ['daniel.oconnor@gmail.com']
+  gem.files = Dir["{app,config,db,lib,vendor,public,bin,log/script}/**/*", "MIT-LICENSE", "Rakefile", "README.md", "config.ru", "CHANGELOG.md", "CONTRIBUTING.md"]
+  gem.version = ''
+  gem.required_ruby_version = '>= 3.1'
+  gem.license = 'MIT'
+
+  gem.add_dependency 'ffcrm_endpoint'
+end
---
+++ ffcrm_webhook_read_ai/handler.rb
@@ -0,0 +1,69 @@
+# frozen_string_literal: true
+require 'ffcrm_endpoint'
+
+class FfcrmWebookhookReadAi::MeetingEndpoint < FfcrmEndpoint::Endpoint
+
+  def process
+    payload = JSON.parse(request.body.read)
+    owner_email = payload.dig("owner", "email")
+    user = User.find_by(email: owner_email)
+
+    return if user.nil?
+
+    ActiveRecord::Base.transaction do
+      payload["participants"].each do |participant|
+        participant["email"]
+        lead_or_contact = find_or_create_lead_or_contact(participant, user)
+
+        next unless lead_or_contact
+
+        # Create a Note
+        lead_or_contact.notes.create(
+          user: user,
+          note: "Meeting Summary: #{payload['summary']}\n\nReport URL: #{payload['report_url']}"
+        )
+      end
+
+      payload["action_items"].each do |action_item|
+        Task.create(
+          user: user,
+          name: action_item["text"],
+          assigned_to: user.id
+        )
+      end
+    end
+  end
+
+  #
+  # return true only if allowed to proceed.
+  def authenticate
+    # TODO: Refactor to a better approach or whitelist.
+    # params[:token].present? && params[:token] == Setting.my_custom_endpoint[:token]
+    request.remote_ip == '127.0.0.1'
+  end
+
+  def find_or_create_lead_or_contact(participant, user)
+    contact = Contact.find_by(email: participant["email"])
+    return contact if contact
+
+    lead = Lead.find_by(email: participant["email"])
+    return lead if lead
+
+    Lead.create(
+      first_name: participant["name"].split.first,
+      last_name: participant["name"].split.last,
+      email: participant["email"],
+      user: user
+    )
+  end
+end
+
+
+
+
+  def verify_ip
+    
+
+    head :unauthorized
+  end
+end
